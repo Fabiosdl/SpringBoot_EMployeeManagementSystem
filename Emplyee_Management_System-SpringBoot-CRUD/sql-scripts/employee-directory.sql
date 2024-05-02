92b8852cc0c27f48010342bd2a676b42eca489bb
Fabiosdl <118284020+Fabiosdl@users.noreply.github.com>
2024-05-02 17:09:20 +0200

    Update employee-directory.sql

--- Emplyee_Management_System-SpringBoot-CRUD/sql-scripts/employee-directory.sql
+++ Emplyee_Management_System-SpringBoot-CRUD/sql-scripts/employee-directory.sql
@@ -14,15 +14,3 @@ CREATE TABLE `employee` (
   `email` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
-
---
--- Data for table `employee`
---
-
-INSERT INTO `employee` VALUES 
-	(1,'Leslie','Andrews','leslie@luv2code.com'),
-	(2,'Emma','Baumgarten','emma@luv2code.com'),
-	(3,'Avani','Gupta','avani@luv2code.com'),
-	(4,'Yuri','Petrov','yuri@luv2code.com'),
-	(5,'Juan','Vega','juan@luv2code.com');
-
